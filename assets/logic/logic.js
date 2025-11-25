fb71cacda2b1451c0139742c88f434155ed1003a
JT <jstorbit@gmail.com>
2025-11-24 22:02:55 -0700

    Add state

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,10 +1,15 @@
+const state = {
+  starters: []
+}
+
 const API = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=9`
 
 const getStarterPokemon = async () => {
   const response = await fetch(API);
   const pokemonData = await response.json();
   const starters = pokemonData.results;
-  console.log(starters);
+  state.starters = starters;
+  console.log(state.starters);
 }
 
 getStarterPokemon();
